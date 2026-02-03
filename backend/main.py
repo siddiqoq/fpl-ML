028401aa4a4b32b86722ea866f74e321a78704b5
Adam Siddiq <adamsiddiq.sidki@gmail.com>
2026-02-03 15:59:28 +0000

    Fixed issue where frontend queries didn't affect backend response

--- backend/main.py
+++ backend/main.py
@@ -8,13 +8,12 @@ from src.path import MODELS_DIR, PROCESSED_DATA_DIR
 
 
 app = FastAPI()
-
 origins = ["http://localhost:5173",
            "localhost:5173"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:5173","localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
@@ -28,23 +27,17 @@ FEATURE_DS = os.path.join(PROCESSED_DATA_DIR, "ml_features_dataset.csv")
 model = joblib.load(MODEL_PATH)
 df = pd.read_csv(FEATURE_DS)
 
+latest_player_gw = df.groupby("player_id").tail(1).reset_index(drop=True)
+X_next = latest_player_gw[["points_last_gw", "points_last_3_gws"]]
+latest_player_gw[f"predicted_points"] = model.predict(X_next)
+
 @app.get("/")
 def top_10(position:int = 0):
-    lastGW = df["gameweek"].max()
-    predictedGW = lastGW + 1
-
-    latest_player_gw = df.groupby("player_id").tail(1).reset_index(drop=True)
-    X_next = latest_player_gw[["points_last_gw","points_last_3_gws"]]
-
+    copy_of_latest_player_gw = latest_player_gw.copy()
     if position != 0: # filter by position before running model
-        latest_player_gw = latest_player_gw[latest_player_gw["element_type"] == position]
-    latest_player_gw[f"predicted_points"] = model.predict(X_next)
+        copy_of_latest_player_gw = copy_of_latest_player_gw[copy_of_latest_player_gw["element_type"] == position]
 
-
-
-
-
-    predicted_values = (latest_player_gw[["player_id","web_name","short_name","now_cost",f"predicted_points","points_last_gw","ict_index","element_type"]]
+    predicted_values = (copy_of_latest_player_gw[["player_id","web_name","short_name","now_cost",f"predicted_points","points_last_gw","ict_index","element_type"]]
                         .sort_values(f"predicted_points",ascending=False)).reset_index(drop=True)
 
     return predicted_values.head(10).to_dict(orient='records')
